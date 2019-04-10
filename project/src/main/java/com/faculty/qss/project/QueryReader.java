fd0fa6ea39990f825ac4d4fd1cb8a5987c30ed44
Ecaterina Mihaela Chilaboc <dev.c.ecaterina.mihaela@gmail.com>
2019-04-10 21:36:33 +0300

    Created new maven project with existing code

---
+++ project/src/main/java/com/faculty/qss/project/QueryReader.java
@@ -0,0 +1,84 @@
+package com.faculty.qss.project;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.faculty.qss.project.comands.Database;
+import com.faculty.qss.project.comands.TableWithoutLibrary;
+
+public class QueryReader {
+
+	public void readQuery() throws IOException {
+		TableWithoutLibrary table = new TableWithoutLibrary();
+		Database database = new Database();
+		while (true) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String command = reader.readLine();
+			String[] splited = command.toLowerCase().split("\\s+");
+
+			if (splited.length == 4 && splited[0].equals("create") && splited[1].equals("new")
+					&& splited[2].equals("database")) {
+				database.createDatabase(splited[3]);
+			}
+			if (splited.length == 2 && splited[0].equals("show") && splited[1].equals("databases")) {
+				database.showDatabases();
+			}
+			if (splited.length == 4 && splited[0].equals("show") && splited[1].equals("tables")
+					&& splited[2].equals("from")) {
+				database.showTablesFromDatabase(splited[3]);
+			}
+			if (splited.length == 4 && splited[0].equals("change") && splited[1].equals("database")
+					&& splited[2].equals("to")) {
+				database.changeDatabaseName(splited[3]);
+			}
+			if (splited.length == 3 && splited[0].equals("delete") && splited[1].equals("database")) {
+				database.deleteDatabase(splited[2]);
+			}
+			if (database.getDatabaseName() != null) {
+				if (splited.length > 2 && splited[0].equals("create") && splited[1].equals("table")) {
+					String[] queryDivider = command.toLowerCase().split("\\(");
+					String[] firstHalf = queryDivider[0].split(" ");
+					String tableName = firstHalf[firstHalf.length - 1].replace(" ", "");
+					HashMap<String, String> fields = new HashMap<>();
+					String[] fieldsAndTypes = queryDivider[1].split(",");
+					for (String str : fieldsAndTypes) {
+						String[] separator = str.replace(" ", "").replace(")", "").split(":");
+						fields.put(separator[0], separator[1]);
+					}
+					table.createTable(database.getDatabaseName(), tableName, fields);
+				}
+				if (splited.length > 2 && splited[0].equals("insert") && splited[1].equals("into")) {
+					String[] queryDivider = command.split("\\(");
+					String[] firstHalf = queryDivider[0].split(" ");
+					String tableName = firstHalf[firstHalf.length - 1].replace(" ", "");
+					ArrayList<String> values = (ArrayList<String>) Stream.of(queryDivider[1].split(","))
+							.map(elem -> elem.replace(" ", "").replace(")", "")).collect(Collectors.toList());
+					table.insertRecord(database.getDatabaseName(), tableName, values);
+				}
+				if (splited.length > 4 && splited[0].equals("delete") && splited[1].equals("from")
+						&& splited[3].equals("where")) {
+					String tableName = splited[2];
+					String whereClause = command.split("where")[1].replace(" ", "");
+					table.deleteRecord(database.getDatabaseName(), tableName, whereClause);
+				}
+//                UPDATE table_name
+//                SET column1 = value1, column2 = value2, ...
+//                WHERE condition;
+				if (splited.length > 5 && splited[0].equals("update") && command.contains("set")
+						&& command.contains("where")) {
+					String tableName = splited[1];
+					String whereClause = command.split("where")[1].replace(" ", "");
+					String fieldsToBeChanged = command.split("set")[1].split("where")[0].replace(" ", "");
+					table.updateRecord(database.getDatabaseName(), tableName, fieldsToBeChanged, whereClause);
+				}
+			}
+
+		}
+	}
+
+}
---
+++ project/src/main/java/com/faculty/qss/project/comands/Database.java
@@ -0,0 +1,68 @@
+package com.faculty.qss.project.comands;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+public class Database {
+    private String name;
+    private String projectPath = Paths.get("").toAbsolutePath().toString() + "\\src\\main\\resources\\";
+
+    public boolean createDatabase(String name) {
+        if (!new File(projectPath + name).exists()) {
+            new File(projectPath + name).mkdir();
+            return true;
+        }
+        return false;
+    }
+
+    public String getDatabaseName() {
+        return this.name;
+    }
+
+    public boolean changeDatabaseName(String name) {
+        if (new File(projectPath + name).exists()) {
+            this.name = name;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteDatabase(String name) {
+        try {
+            Files.walk(Paths.get(projectPath + name))
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+    public void showDatabases() {
+        printFiles(projectPath);
+    }
+
+    public void showTablesFromDatabase(String databaseName) {
+        printFiles(projectPath + databaseName);
+    }
+
+    private void printFiles(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("Table: " + listOfFiles[i].getName().replace(".xml", ""));
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Database " + listOfFiles[i].getName());
+            }
+        }
+        System.out.println();
+    }
+}
