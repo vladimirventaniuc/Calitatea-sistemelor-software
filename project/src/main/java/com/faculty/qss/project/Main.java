621a20cb6512e903d0d79bda2ea8d75323384716
Ecaterina Mihaela Chilaboc <dev.c.ecaterina.mihaela@gmail.com>
2019-04-20 22:18:55 +0300

    Moved Vlad code to my project..

---
+++ project/src/main/java/com/faculty/qss/project/Main.java
@@ -0,0 +1,10 @@
+package com.faculty.qss.project;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        QueryReader queryReader = new QueryReader();
+        queryReader.readQuery();
+        }
+}
